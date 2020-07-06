984e55c528f059f914608ff10b697415813ae652
Anthony <ajsouza88@gmail.com>
2020-07-06 12:37:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CSC 415 - Project 3 - Producer Consumer
+
+## Student Name : Name goes here
+
+## Student ID : ID goes here
+
+## Build Instructions
+
+## Run Instructions
---
+++ pandc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <semaphore.h>
+
+
+
+/**
+ * for the 2 functions below, look
+ * at the process slides/video for 
+ * sample code that can be used.
+ */
+/* 
+ * Function to remove item.
+ * Item removed is returned
+ */
+int grab_item()
+{
+    
+}
+
+/* 
+ * function to put item
+ * into shared resource 
+ * so it can be consumed
+ */
+void put_item(int item)
+{
+    
+}
+
+int main(int argc. char* argv[]) 
+{
+
+}
